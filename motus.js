668e477e4b32c91daf55b4019c76e5c3727d094d
BDafflon <trysac@gmail.com>
2022-01-11 09:36:23 +0100

    Add files via upload

--- motus.js
+++ motus.js
@@ -2,13 +2,53 @@ var tryUser=[];
 var tryWord=[];
 var maxSize=5;
 var word=""
+var allword=[]
+var maxTry=6;
+var gameFini=false;
 
+var localStorageStat = {
+    "nbPartie":0,
+    "dernierMots":"plume",
+    "grille":[],
+    "distribution":{
+        "1":0,
+        "2":0,
+        "3":0,
+        "4":0,
+        "5":0,
+        "6":0
+    }
 
-function readTextFile(file)
+}
+
+function setObjetn(cle, objet)
 {
-    fetch(file).then(response => response.text())
-    .then(text => console.log(text))
+    window.localStorage.setItem(cle, JSON.stringify(objet));
+}
 
+function getObject(cle)
+{
+ var valeur =  window.localStorage.getItem(cle);
+ return valeur && JSON.parse(valeur);
+}
+
+function saveStat(){
+    setObjetn("stats",localStorageStat);
+}
+
+function setStat(){
+    var nb = getObject("stats");
+    if (nb==null)
+    setObjetn("stats",localStorageStat);
+    else
+        localStorageStat = nb;
+}
+
+function setDict(d){
+    allword=d;
+}
+function getDict2(){
+    return allword;
 }
 
 function setWord(w){
@@ -19,7 +59,7 @@ function getWord(){
 }
 
 function addLetter(l){
-    if(tryWord.length < maxSize){
+    if(tryWord.length < maxSize && gameFini==false){
         
         var elements = document.getElementsByClassName("case"+tryUser.length+tryWord.length);
         console.log(elements)
@@ -33,11 +73,12 @@ function addLetter(l){
 function removeLetter(){
     
     console.log(tryWord)
-    var elements = document.getElementsByClassName("case"+tryUser.length+tryWord.length);
+    if(tryWord.length>0){
+        var elements = document.getElementsByClassName("case"+tryUser.length+(tryWord.length-1));
         console.log(elements)
         elements[0].innerHTML=""
         tryWord.pop();
-
+    }
 }
 
 
@@ -48,20 +89,49 @@ function printGrid(){
 }
 
 
-function addWord(){
-    
+function addWord(save){
+    var win = false;
     if(tryUser.length<6){
         if (tryWord.length == maxSize){
-            checkWord()
-            tryUser.push(tryWord);
-            tryWord=[];
+            if(allword.includes(tryWord.join(''))){
+                
+                win = checkWord()
+				if(save){
+                    localStorageStat["grille"].push(tryWord)
+                    saveStat()
+					 
+                }
+                tryUser.push(tryWord);
+                tryWord=[];
+            }
         }
     }
-    return tryUser.length;
+
+    if (win || tryUser.length==6){
+         if(save){
+			localStorageStat["distribution"][tryUser.length+""]=localStorageStat["distribution"][tryUser.length+""]+1
+            localStorageStat["nbPartie"]=localStorageStat["nbPartie"]+1
+			saveStat()
+			
+		 }
+		 
+    }
+
+    return [tryUser.length,win];
 
 }
 
+function gameOver(){
+    console.log(word)
+    for(var i=0;i<5;i++){
+        var elements = document.getElementsByClassName("case"+i);
+        elements[0].classList.add("okCase");      
+        elements[0].innerHTML = word.charAt(i)   
+    }
+}
+
 function checkWord(){
+    var find=maxSize;
     tryWord.forEach((l ,i)=> {
         if(getWord().includes(l)){
             if(getWord().charAt(i)!=tryWord[i]){
@@ -81,6 +151,7 @@ function checkWord(){
                 elements[0].classList.remove("notokCase");
                 elements[0].classList.add("okCase");
                 console.log("IN",elements)
+                find=find-1
             }
             
         }
@@ -95,7 +166,98 @@ function checkWord(){
         }
         
     });
+    if (find==0)
+        gameFini=true;
+    
+    return find==0;
     
     
 }
- 
+
+
+
+function share(){
+	var shareString=""
+	localStorageStat["grille"].forEach(l=>{
+		l.forEach((c,i)=>{
+			if(getWord().includes(c)){
+				if(getWord().charAt(i)!=c)
+					shareString+="🟨"
+				else
+					shareString+="🟩"
+			}
+			else{
+				shareString+="⬜"
+			}
+		})
+		shareString+="\n"
+	})
+
+     
+   /* Copy the text inside the text field */
+   copyToClipboard(shareString)
+   .then(() => console.log('text copied !'))
+   .catch(() => console.log('error'));
+	alert("copier dans le presspapier")
+	console.log(shareString)
+}
+
+function loadStat(){
+
+	
+    var stat = localStorageStat;
+	
+    var elements = stat["nbPartie"];
+	elements = document.getElementsByClassName("partiejouees");
+    elements[0].innerHTML=stat["nbPartie"];
+
+    nb = stat["distribution"][1];
+    elements = document.getElementsByClassName("nbPartie1");
+    elements[0].innerHTML=nb;
+
+    nb = stat["distribution"][2];
+    elements = document.getElementsByClassName("nbPartie2");
+    elements[0].innerHTML=nb;
+
+    nb = stat["distribution"][3];
+    elements = document.getElementsByClassName("nbPartie3");
+    elements[0].innerHTML=nb;
+
+    nb = stat["distribution"][4];
+    elements = document.getElementsByClassName("nbPartie4");
+    elements[0].innerHTML=nb;
+
+    nb = stat["distribution"][5];
+    elements = document.getElementsByClassName("nbPartie5");
+    elements[0].innerHTML=nb;
+
+    nb = stat["distribution"][6];
+    elements = document.getElementsByClassName("nbPartie6");
+    elements[0].innerHTML=nb;
+
+
+}
+
+function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
